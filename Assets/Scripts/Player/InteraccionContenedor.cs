fda91b3f8a151f1276e0e67eb167f86c23f52cca
IvoTab11 <ivoagusmitasil@gmail.com>
2025-02-01 23:04:23 -0300

    Mecanica tacho de basura funcionando

---
+++ Assets/Scripts/Player/InteraccionContenedor.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+public class InteraccionContenedor : MonoBehaviour
+{
+    //Verifica si el personaje está colisionando con el contenedor.
+    private bool enContenedor = false;
+    private AlcoholBar bubble;
+
+    void Start(){
+        bubble = FindAnyObjectByType<AlcoholBar>();
+    }
+
+    //Compara los tags y verifica si el personaje está colisionando con el tag "Contenedor".
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Contenedor"))
+        {
+            enContenedor = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Contenedor"))
+        {
+            enContenedor = false;
+        }
+    }
+
+    //Verifica si el booleano es verdadero y si se está presionando la tecla "E".
+    void Update()
+    {
+        if (enContenedor && Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(VaciarBurbujas());
+            //bubble.PerderBubble();
+           // Debug.Log("El personaje está interactuando con el contenedor.");
+        }
+    }
+
+    private IEnumerator VaciarBurbujas()
+    {
+        while (bubble.bubblesCant > AlcoholBar.MinBubbles){
+            bubble.PerderBubble();
+            Debug.Log("El personaje está interactuando con el contenedor.");
+            yield return new WaitForSeconds(0.25f);
+        }
+        
+    }
+}
