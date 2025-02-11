5079969080b0205df2f29e23e51b61524cececee
ValdezCruzLean <143297413+ValdezCruzLean@users.noreply.github.com>
2025-02-11 17:10:11 -0300

    El jugador de puede esconder

--- Assets/GameManager/PlayerMovements2.cs
+++ Assets/GameManager/PlayerMovements2.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovements : MonoBehaviour
+public class PlayerMovements2 : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 60f;
-    public float coyoteTime = 0.2f; // Tiempo para saltar despues de tocar piso
+    public float coyoteTime = 0.2f;
     private float coyoteTimer;
 
     private Rigidbody2D rb;
@@ -14,99 +14,63 @@ public class PlayerMovements : MonoBehaviour
     private bool jumpPressed;
     private bool facingRight = true;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
-    /*[SerializeField] private LayerMask queEsSuelo;
-    [SerializeField] private Transform controladorTecho;
-    [SerializeField] private float radioTecho;
-    [SerializeField] private float multiplicadorVelocidadAgachado;
-    [SerializeField] private Collider2D colisionadorAgachado;
-    private bool estabaAgachado = false;
-    private bool agachar = false;*/
     public CircleCollider2D circleCollider;
     public float StandingSize;
     public float CrouchingSize;
+
+    private bool enEscondite = false;
+    private bool ocultandose = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         circleCollider = GetComponent<CircleCollider2D>();
-        //circleCollider.radius = StandingSize;
-
-        StandingSize = circleCollider.radius; 
+        StandingSize = circleCollider.radius;
     }
 
     void Update()
     {
-
-        float move = Input.GetAxisRaw("Horizontal"); // Movimiento horizontal del personaje
-        //float move2 = Input.GetAxisRaw("Vertical"); //Movimiento vertical del personaje
+        float move = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
 
         animator.SetBool("caminando", move != 0);
 
-        /* Condicional para voltear el sprite del personaje*/
         if (move > 0 && !facingRight)
             Flip();
         else if (move < 0 && facingRight)
             Flip();
 
-        /* Condicioal para detectar el slato del personaje*/
         if (Input.GetButtonDown("Jump"))
             jumpPressed = true;
-        
-        
-        if(Input.GetKeyDown(KeyCode.C))
-        {
+
+        if (Input.GetKeyDown(KeyCode.C))
             circleCollider.radius = CrouchingSize;
-        }
-        if(Input.GetKeyUp(KeyCode.C))
-        {
+        if (Input.GetKeyUp(KeyCode.C))
             circleCollider.radius = StandingSize;
-        }
-        /*if(move2 < 0)
+
+        if (enEscondite && Input.GetKey(KeyCode.E))
         {
-            agachar = true;
+            ocultandose = true;
+            SetTransparency(0.2f);
         }
         else
         {
-            agachar = false;
+            ocultandose = false;
+            SetTransparency(1f);
         }
-
-        if(!agachar)
-        {
-            if(Physics2D.OverlapCircle(controladorTecho.position, radioTecho, queEsSuelo))
-            {
-                agachar = true;
-            }
-        }
-        if(agachar){
-            if(!estabaAgachado){
-                estabaAgachado = true;
-            }
-            
-            move *= multiplicadorVelocidadAgachado;
-            colisionadorAgachado.enabled = false;
-        }
-        else
-        {
-            colisionadorAgachado.enabled = true;
-
-            if(estabaAgachado)
-            {
-                estabaAgachado = false;
-            }
-        }*/
     }
 
     void FixedUpdate()
     {
-
         if (isGrounded)
             coyoteTimer = coyoteTime;
         else
             coyoteTimer -= Time.fixedDeltaTime;
-
 
         if (jumpPressed && coyoteTimer > 0)
         {
@@ -117,14 +81,13 @@ public class PlayerMovements : MonoBehaviour
 
         jumpPressed = false;
     }
-    /*Metodo que cambia la direccion que mira el personaje.*/
+
     void Flip()
     {
         facingRight = !facingRight;
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
-    /*Metodo para detectar cuando el personaje toca el suelo usando el tag Ground*/
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("suelo"))
@@ -135,5 +98,34 @@ public class PlayerMovements : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("suelo"))
             isGrounded = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Escondite"))
+        {
+            enEscondite = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Escondite"))
+        {
+            enEscondite = false;
+            SetTransparency(1f);
+        }
+    }
+
+    void SetTransparency(float alpha)
+    {
+        Color color = spriteRenderer.color;
+        color.a = alpha;
+        spriteRenderer.color = color;
+    }
+
+    public bool EstaOculto()
+    {
+        return ocultandose;
     }
 }
