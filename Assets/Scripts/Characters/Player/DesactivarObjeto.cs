b17469fe92b401f2696eec758c75ab366b0a4341
IvoTab11 <ivoagusmitasil@gmail.com>
2025-02-13 20:22:02 -0300

    Problema con la desaparición del objeto solucionado

--- Assets/Scripts/Characters/Player/DesactivarObjeto.cs
+++ Assets/Scripts/Characters/Player/DesactivarObjeto.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class DesactivarObjeto : MonoBehaviour
 {
+    private bool colisionoConSuelo = false;
     public GameObject objeto;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("suelo"))
+        /*if (collision.gameObject.layer == LayerMask.NameToLayer("suelo"))
         {
+            StartCoroutine(DesactivarElemento());
+        }*/
+        if (collision.gameObject.CompareTag("suelo") && !colisionoConSuelo)
+        {
+            colisionoConSuelo = true;
+            Debug.Log("Colisionó con el suelo. Se desactivará en 5 segundos.");
             StartCoroutine(DesactivarElemento());
         }
     }
